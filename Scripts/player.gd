9193721cf0dad87c9e50117a09ec01f914f4eb09
vinimaxfr-beep <davireiss1409+github@gmail.com>
2026-05-12 11:38:18 -0300

    implementar fase

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,12 +3,24 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 var direcao = Vector2.ZERO
+@onready var ponta_arma = $PontaDaArma
+@export var bala_cena: PackedScene
 
 func _physics_process(delta: float) -> void:
 	mover()
 	rotacionar_corpo()
-
+	
 	move_and_slide()
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		disparar()
+		
+		
+func disparar():
+	var nova_bala = bala_cena.instantiate()
+	nova_bala.global_position = ponta_arma.global_position
+	get_tree().current_scene.add_child(nova_bala)
 	
 func mover():
 	
