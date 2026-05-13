f7a3fd9d6f771046500aa4188cbee88aa9c6f15e
vinimaxfr-beep <davireiss1409+github@gmail.com>
2026-05-13 11:40:48 -0300

    som do jogo

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 var direcao = Vector2.ZERO
 @onready var ponta_arma = $PontaDaArma
 @export var bala_cena: PackedScene
+@onready var som_tiro = $somdetiro
 
 func _physics_process(delta: float) -> void:
 	mover()
@@ -13,18 +14,23 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("atirar"):
 		disparar()
 		
 		
 func disparar():
 	var nova_bala = bala_cena.instantiate()
 	nova_bala.global_position = ponta_arma.global_position
+	nova_bala.direcao = (ponta_arma.global_position - global_position). normalized()
+	nova_bala.rotation = (rotation)
 	get_tree().current_scene.add_child(nova_bala)
+	som_tiro.play(0.4)
+	await get_tree().create_timer(0.7).timeout
+	som_tiro.stop()
 	
 func mover():
 	
-	direcao = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	direcao = Input.get_vector("andar_esquerda", "andar_direita", "andar_cima", "andar_baixo")
 	velocity = direcao * SPEED
 	
 func rotacionar_corpo():
